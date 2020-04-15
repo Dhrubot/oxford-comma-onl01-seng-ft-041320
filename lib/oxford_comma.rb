701a6d006fbc3541184355011d1979fe87e24da2
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-15 19:49:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,18 @@
 def oxford_comma(array)
-  array.join
-  array.join(" and ")
+  
+  if array.length == 1
+    array.join
+    
+  elsif array.length == 2
+    array.join(" and ")
+    
+  elsif array.length == 3
   array[-2] << " and "
   array.join
+  
+ else
+   array[-2] << "and"
+   array.join(" ")
+ end
+   
 end
